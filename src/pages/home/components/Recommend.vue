7a3b2e6c14f557da307074f1903cd38efbb80985
CN-sharon <2293606706@qq.com>
2018-09-08 19:22:37 +0800

    add recommend and weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="(item,index) of itemList" :key="index">
+        <img class="item-img" :src="item.imgUrl">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      itemList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_200x200_ea02ef99.jpg',
+        title: '长隆野生动物世界',
+        desc: '夏日缤纷，与家人好友结伴而行，长隆野生动物世界欢迎您'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1707/47/477416df92210439a3.img.jpg_200x200_b9e4ac2c.jpg',
+        title: '长隆欢乐世界',
+        desc: '和家人一起游览各种动物'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/9e/7b5e0a44cb0310280b1aeb2a8dd94de8.water.jpg_200x200_3c429960.jpg',
+        title: '长隆国际大马戏剧院',
+        desc: '夏日缤纷，与家人好友结伴而行，长隆野生动物世界欢迎您'
+      }, {
+        id: '004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1707/ab/abb70c851caaa6d8a3.img.jpg_200x200_b902dd9e.jpg',
+        title: '长隆水上乐园',
+        desc: '和家人一起游览各种动物'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixin.styl'
+
+  .recommend-title
+    line-height: .8rem
+    margin-top: .2rem
+    text-indent: .2rem
+    background: #eee
+  .item
+    overflow: hidden
+    height: 2.1rem
+    display: flex
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding :.2rem
+    .item-info
+      flex:1
+      min-width: 0
+      padding :.1rem .1rem .1rem 0
+      .item-title
+        font-size: .32rem
+        line-height:.45rem
+        padding-top .2rem
+        ellipsis()
+      .item-desc
+        line-height:.56rem
+        color:#ccc
+        ellipsis()
+      .item-button
+        padding: .05rem .1rem
+        line-height: .4rem
+        color: #ffffff
+        background: #ffbe1f
+        border-radius: .06rem
+</style>
---
+++ src/pages/home/components/weekend.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="weekend-title">周末去哪儿</div>
+    <ul>
+      <li class="item" v-for="(item,index) of itemList" :key="index">
+        <img class="item-img" :src="item.imgUrl">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      itemList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/92/580e9ea4f37a1b.jpg_r_640x214_72112761.jpg',
+        title: '广州必游TOP10',
+        desc: '感受现代与传统相融合的广州'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/50/07fa9343791682.jpg_r_640x214_c8c3d798.jpg',
+        title: '自然户外好去处',
+        desc: '快快来到户外，感受花城的魅力'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/ec/902725c4d56517.jpg_r_640x214_7a74d251.jpg',
+        title: '羊城民俗风情游',
+        desc: '品味羊城文化，感受羊城别样风情'
+      }, {
+        id: '004',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/6f/bf9ef732f8ad2e.jpg_r_640x214_c759559e.jpg',
+        title: '宝贝亲子游乐地',
+        desc: '宝贝闲不住，我们去哪儿呀'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixin.styl'
+
+  .weekend-title
+    line-height: .8rem
+    margin-top: .2rem
+    text-indent: .2rem
+    background: #eee
+  .item
+    .item-img
+      width: 100%
+    .item-info
+      padding :.1rem
+      .item-title
+        font-size: .32rem
+        line-height:.45rem
+        padding-top .2rem
+        ellipsis()
+      .item-desc
+        line-height:.56rem
+        color:#ccc
+        ellipsis()
+      .item-button
+        padding: .05rem .1rem
+        line-height: .4rem
+        color: #ffffff
+        background: #ffbe1f
+        border-radius: .06rem
+</style>
