fc16c2e90aa0c5f21c6b9bf64ffa192a9afacaba
CN-sharon <2293606706@qq.com>
2018-09-10 19:42:58 +0800

    change list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,36 +1,26 @@
 <template>
   <div>
-<<<<<<< HEAD
-    <CityHeader></CityHeader>
-    <CitySearch></CitySearch>
-=======
     <city-header></city-header>
     <city-search></city-search>
     <city-list></city-list>
     <city-alphabet></city-alphabet>
->>>>>>> origin/city-list
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
-<<<<<<< HEAD
-=======
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
->>>>>>> origin/city-list
+
 export default {
   name: 'City',
   components: {
     CityHeader,
-<<<<<<< HEAD
-    CitySearch
-=======
     CitySearch,
     CityList,
     CityAlphabet
->>>>>>> origin/city-list
+
   }
 }
 </script>
