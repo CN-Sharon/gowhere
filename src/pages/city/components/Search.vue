107c648b4543e031df20ed6d5b6934924f377a09
CN-sharon <2293606706@qq.com>
2018-09-11 00:11:47 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,13 +1,55 @@
 <template>
-  <div class="search">
-      <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+        <input v-model="keyworld" class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" v-show="keyworld">
+      <p class="item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</p>
+      <p class="item border-bottom" v-show="hasNoData">没有找到数据</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CitySearch'
-
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyworld: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyworld () {
+      if (this.timer) {
+        setTimeout(this.timer)
+      }
+      if (!this.list) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyworld) > -1 || value.name.indexOf(this.keyworld) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 
@@ -26,4 +68,18 @@ export default {
       border-radius: 0.06rem
       color:#666
       background:#fff
+  .search-content
+    position: absolute
+    top:1.56rem
+    left:0
+    right:0
+    bottom:0
+    z-index:1
+    background: #ccc
+    .item
+      line-height: .56rem
+      text-indent :.2rem
+      color:#666
+      background :#fff
+
 </style>
